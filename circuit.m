fb3bfd3d0f084865f94c23ab8bfe63eca8cd769e
Sarah Li <sarah_li_7@sfu.ca>
2023-03-18 13:04:09 -0700

    Create circuit.m

---
+++ circuit.m
@@ -0,0 +1,21 @@
+%x = inv(A)*b
+%x = A/b
+%x = pinv(A)*b
+%c = Ax
+clear all;
+close all;
+A = [1 0 0 0 2000 0 0 0 0 0;
+     1 -1 ]
+[L,U,P] = lu (A); %Apply LU decomposition
+n = size(L);
+% Forward Substitution
+d = zeros(n,1);
+d(1) = ;
+    for i = 2 : 1 : n
+        sum = 0;
+        for k = 1:1:(i-1)
+            sum = %insert eqn;
+        end
+        di = %insert eqn;
+    end 
+% Backward Substitution
